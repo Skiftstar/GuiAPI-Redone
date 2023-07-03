429e63719064bfde25dcb95f591dcd230194b32c
Kyu <46356129+Skiftstar@users.noreply.github.com>
2023-07-03 22:50:48 +0200

    move docs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Kyu.GuiAPI_Redone"},{"l":"Kyu.GuiAPI_Redone.Exceptions"},{"l":"Kyu.GuiAPI_Redone.Item"},{"l":"Kyu.GuiAPI_Redone.Testing"},{"l":"Kyu.GuiAPI_Redone.Window"},{"l":"Kyu.GuiAPI_Redone.Window.WindowImpl"},{"l":"Kyu.GuiAPI_Redone.Window.WindowImpl.MultiWindow"}];updateSearchResults();
